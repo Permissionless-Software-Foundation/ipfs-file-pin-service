1cd9327e83cc629a33086fc2c6b1c2f47805f89a
Chris Troutner <chris.troutner@gmail.com>
2024-05-11 13:38:33 -0700

    Restoring template start-production.sh

--- production/docker/start-production.sh
+++ production/docker/start-production.sh
@@ -5,18 +5,10 @@
 # This mnemonic is used to set up persistent public key for e2ee
 # Replace this with your own 12-word mnemonic.
 # You can get one at https://wallet.fullstack.cash.
-#export MNEMONIC="olive two muscle bottom coral ancient wait legend bronze useful process session"
+export MNEMONIC="olive two muscle bottom coral ancient wait legend bronze useful process session"
 
 # The human readable name this IPFS node identifies as.
-#export COORD_NAME=ipfs-file-pin-service
-
-# This mnemonic is used to set up persistent public key for e2ee
-# Replace this with your own 12-word mnemonic.
-# You can get one at https://wallet.fullstack.cash.
-export MNEMONIC="absorb useful protect assist scrap pledge dolphin dove primary iron tiny estate"
-
-# The human readable name this IPFS node identifies as.
-export COORD_NAME=ipfs-file-pin-dev-decatur
+export COORD_NAME=ipfs-file-pin-service
 
 
 # Allow this node to function as a circuit relay. It must not be behind a firewall.
@@ -45,9 +37,9 @@ export IPFS_WS_PORT=4003
 # 0 = no debug logs. 3 = maximum debug logs.
 export DEBUG_LEVEL=0
 
-# Use local instance of bch-ap
-export WALLET_INTERFACE=web2
-export APISERVER=http://172.17.0.1:3000/v5/
+# Use local instance of bch-api
+#export WALLET_INTERFACE=web2
+#export APISERVER=http://172.17.0.1:3000/v5/
 
 
 npm start
