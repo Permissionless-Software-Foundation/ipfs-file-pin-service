ec40a6230ff8e1b8c81324c765bbfe865c56ba47
Chris Troutner <chris.troutner@gmail.com>
2024-05-11 04:06:57 +0100

    Trying to add retry queue back in

--- production/docker/start-production.sh
+++ production/docker/start-production.sh
@@ -5,10 +5,19 @@
 # This mnemonic is used to set up persistent public key for e2ee
 # Replace this with your own 12-word mnemonic.
 # You can get one at https://wallet.fullstack.cash.
-export MNEMONIC="olive two muscle bottom coral ancient wait legend bronze useful process session"
+#export MNEMONIC="olive two muscle bottom coral ancient wait legend bronze useful process session"
 
 # The human readable name this IPFS node identifies as.
-export COORD_NAME=ipfs-file-pin-service
+#export COORD_NAME=ipfs-file-pin-service
+
+# This mnemonic is used to set up persistent public key for e2ee
+# Replace this with your own 12-word mnemonic.
+# You can get one at https://wallet.fullstack.cash.
+export MNEMONIC="absorb useful protect assist scrap pledge dolphin dove primary iron tiny estate"
+
+# The human readable name this IPFS node identifies as.
+export COORD_NAME=ipfs-file-pin-dev-decatur
+
 
 # Allow this node to function as a circuit relay. It must not be behind a firewall.
 #export ENABLE_CIRCUIT_RELAY=true
@@ -35,5 +44,10 @@ export IPFS_WS_PORT=4003
 # Set the debug level for helia-coord. 0-3.
 # 0 = no debug logs. 3 = maximum debug logs.
 export DEBUG_LEVEL=0
+
+# Use local instance of bch-ap
+export WALLET_INTERFACE=web2
+export APISERVER=http://172.17.0.1:3000/v5/
+
 
 npm start
