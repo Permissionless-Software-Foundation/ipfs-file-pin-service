aea39553ea4a0ccf8dcd349a28e29ecc35b16437
Chris Troutner <chris.troutner@gmail.com>
2025-03-29 14:03:38 -0700

    fix(processPinClaim()): Reducing processing wait time from 30 sec to 10 sec

--- src/use-cases/ipfs.js
+++ src/use-cases/ipfs.js
@@ -110,8 +110,8 @@ class IpfsUseCases {
 
       // Wait a few seconds to ensure the TXs have syndicated and been processed
       // by the infrastructure.
-      console.log('Waiting 30 seconds to let TXs get processed...')
-      await this.wallet.bchjs.Util.sleep(30000)
+      console.log('Waiting 10 seconds to let TXs get processed...')
+      await this.wallet.bchjs.Util.sleep(10000)
 
       // Get TX details for the proof-of-burn TX.
       let pobTxDetails = await this.wallet.getTxData([proofOfBurnTxid])
